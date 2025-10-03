2d292dd5b6342fe70aa9ee2ea5ac0d143bd7c683
Alice0310 <alice20020310@icloud.com>
2025-10-03 22:18:07 +0900

    ログイン時、トップページをスタイリトボタンに

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,8 +12,16 @@
         <h1 class="hero-title">CordiMatch</h1>
         <p class="hero-subtitle">コーディネートをもっと楽しく、もっと身近に。</p>
         <div class="hero-buttons">
-            <a href="{{ route('register') }}" class="btn btn-accent">会員登録</a>
-            <a href="{{ route('login') }}" class="btn btn-outline">ログイン</a>
+            {{-- 未ログイン時 --}}
+            @guest
+                <a href="{{ route('register') }}" class="btn btn-accent">会員登録</a>
+                <a href="{{ route('login') }}" class="btn btn-outline">ログイン</a>
+            @endguest
+
+            {{-- ログイン時 --}}
+            @auth
+                <a href="{{ url('/stylist/become') }}" class="btn btn-accent">スタイリストになる</a>
+            @endauth
         </div>
     </div>
 </section>
